9ebc2e124e23c19493e686c83866c76a9783672b
Yhomarth <yhomarth@gmail.com>
2019-09-11 08:53:03 -0400

    Ejercicios completados hasta la clase 82

---
+++ inv/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+
+from .models import Categoria, SubCategoria
+
+
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['descripcion', 'estado']
+        labels = {'descripcion' : 'Descripcion de la categoria', 
+                    'estado' : 'Estado' }
+        widget = { 'descripcion' : forms.TextInput }        
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+
+class SubCategoriaForm(forms.ModelForm):
+    class Meta:
+        model = SubCategoria
+        fields = ['categoria','descripcion', 'estado']
+        labels = {'descripcion' : 'Sub Categoria', 
+                    'estado' : 'Estado' }
+        widget = { 'descripcion' : forms.TextInput }        
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+
+        self.fields['categoria'].empty_label = 'Seleccione Categoria'
+        
---
+++ inv/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+from bases.models import ClaseModelo
+
+# Create your models here.
+
+class Categoria(ClaseModelo):
+    
+    descripcion = models.CharField(
+        max_length = 100,
+        help_text = 'Descripcion de la categoria',
+        unique = True
+    )
+
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Categoria, self).save()
+
+    
+    class Meta:
+        verbose_name_plural = "Categorias"
+
+
+class SubCategoria(ClaseModelo):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length = 100,
+        help_text = 'Descripcion de la subcategoria'
+    )
+
+    def __str__(self):
+        return '{} : {}'.format(self.categoria.descripcion, self.descripcion)
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(SubCategoria, self).save()
+
+
+    class Meta:
+        verbose_name_plural = "SubCategorias"
+        unique_together = ('categoria', 'descripcion')
+        
